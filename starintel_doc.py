de63fd0e70441b038bfbc0634e2baf91c53f41ce
N545PY <nsaspy@airmail.cc>
2022-02-20 12:17:55 -0500

    yet another sleepy mistake

--- starintel_doc.py
+++ starintel_doc.py
@@ -23,7 +23,7 @@ class BookerDocument:
     date_added: str = field(default="Star Intel", kw_only=True)
 
     def make_id(self, json: str) -> str:
-        return sha256(bytes(json, encoding="unicode")).hexdigest()
+        return sha256(bytes(json, encoding="utf-8")).hexdigest()
 @dataclass
 class BookerPerson(BookerDocument):
     fname: str =field(kw_only=True)
