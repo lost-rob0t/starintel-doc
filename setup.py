14fd6cb0aef1badb0c469a8194a5cf3f649d6feb
N545PY <nsaspy@airmail.cc>
2022-04-28 03:19:01 -0400

    package issues

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 import re
 PACKAGE_NAME = 'starintel_doc'
-SOURCE_DIRECTORY = 'src'
+SOURCE_DIRECTORY = './src'
 SOURCE_PACKAGE_REGEX = re.compile(rf'^{SOURCE_DIRECTORY}')
 
 source_packages = find_packages(include=[SOURCE_DIRECTORY, f'{SOURCE_DIRECTORY}.*'])
 proj_packages = [SOURCE_PACKAGE_REGEX.sub(PACKAGE_NAME, name) for name in source_packages]
 setup(
-    name="starintel_doc",
-    version="0.4.0",
+    version="0.4.2",
     description="Document Spec for Star intel",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://gitlab.com/unseen-giants/starintel_doc",
     author="Nsaspy",
     license="MIT",
-    py_modules=["starintel_doc"],
+    name=PACKAGE_NAME,
+    packages=proj_packages,
+    package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
