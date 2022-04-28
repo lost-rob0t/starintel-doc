7d8cb2df63223d1f28f48534c1aa67226e98602a
N545PY <nsaspy@airmail.cc>
2022-04-28 04:21:17 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="starintel_doc",
-    version="0.4.2",
+    version="0.4.5",
     description="Document Spec for Star intel",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://gitlab.com/unseen-giants/starintel_doc",
     author="Nsaspy",
     license="MIT",
-    py_modules=["starintel_doc", "exceptions"],
+    py_modules=["starintel_doc", "star_exceptions"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
