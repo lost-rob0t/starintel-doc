f4127f7926830794131b9b77ee8ac5883e4a2204
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-25 17:41:40 -0400

    Release Python spec as v0.9.0

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
 setup(
     name="starintel_doc",
-    version="0.8.2",
-    description="Document Spec for Star intel",
+    version="0.9.0",
+    description="Canonical Python runtime for StarIntel document schema v0.9.0",
     long_description_content_type="text/markdown",
     url="https://github.com/lost-rob0t/starintel-doc",
     packages=find_packages(),
     install_requires=["ulid-py", "dataclasses-json"],
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
